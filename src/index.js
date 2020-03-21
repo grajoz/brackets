1ceaa165f37262891836e6988b4811975fb42849
grajoz <grajoz@yandex.ru>
2020-03-21 20:04:19 +0400

    feat: add breckets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let op;
+  let clo;
+   
+  let strArrControl=[];
+  let strArr=[];
+  strArr=str.split('');
+  console.log(strArr);
+  
+ 
+  for (j=0;j<bracketsConfig.length;j++){
+      
+      
+            for (i=0;i<strArr.length;i++){
+              
+              
+              if(strArr[i]==bracketsConfig[j][0]||strArr[i]==bracketsConfig[j][1]){
+                c
+                strArrControl.push(strArr[i]);
+              }
+            }
+      
+      if (strArrControl.length%2!==0){return false};
+  
+  
+      while (strArrControl.length ==0){
+          for (i=1;i<strArrControl.length;i++){
+            if(strArrControl[i]==strArrControlr[i-1]){
+              strArrControl.splice(i-1, 2);
+            }else{return false};
+  
+            
+      }
+      }
+  }
+  return true
+  
+    
+    // your solution
+  }
+  
